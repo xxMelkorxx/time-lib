655598bfeaf9aae89577669f9e38872827d2c511
SerBor <xxxserborxxx@gmail.com>
2023-03-27 09:07:40 +0300

    Орфографический фикс

--- time-lib/main.cpp
+++ time-lib/main.cpp
@@ -8,12 +8,6 @@ int main()
 
 	string init_time, time_str;
 
-	CODE_RESULT result1 = GetTimePlus5MinStr("\t2:22\t", time_str);
-	cout << result1 << " " << time_str << endl;
-
-	/*CODE_RESULT result2 = GetTimePlus5MinStr("1 3:33", time_str);
-	cout << result2 << " " << time_str << endl;*/
-
 	while (true) {
 		cout << "Введите время в формате «ЧАС:МИН»" << endl;
 		cin >> init_time;
